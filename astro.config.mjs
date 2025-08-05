684e4cfeac41344cdc5d27fd6df0a290e5eb3af0
David Tully <tullyd43@gmail.com>
2025-08-05 11:27:47 -0400

    fix for rollup netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,13 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lockedinlocksmith.com',
-  integrations: [partytown(), sitemap()]
+  integrations: [partytown(), sitemap()],
+	vite: {
+		// The `ssr.external` option is not needed to solve the Netlify build error.
+		// build: {
+		// 	rollupOptions: {
+		// 		// ...
+		// 	},
+		// },
+	},
 });
